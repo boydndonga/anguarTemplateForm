829a66a38c9bf61bb0ca093c94fad856fb253ec4
Boyd Ndonga <boydbugle@gmail.com>
2018-08-11 00:40:07 +0300

    create seed data for quote

---
+++ src/app/quotes.ts
@@ -0,0 +1,7 @@
+import { Quote } from './quote';
+
+export let Quotes: Quote[] =[
+  new Quote(1, 'Boyd', 'this is quote1', 'look inspired by this quote'),
+  new Quote(2, 'Boydy', 'this is quote2', 'look amazed by this quote'),
+  new Quote(4, 'Boyddy', 'this is quote3', 'node towards this quote')
+];
